74e313b611a02ce9579725b667c6f3b17c02d6ed
Joachim Renard <joachim.renard@student.vinci.be>
2021-03-06 12:12:24 +0100

    Refactor: amelioration error message login

--- webApp/src/Components/PrintError.js
+++ webApp/src/Components/PrintError.js
@@ -1,11 +1,17 @@
 const PrintError = (err) => {
   if (!err) return;
   let page = document.querySelector("#page");
-  const errorDiv = document.createElement("div");
-  errorDiv.className = "alert alert-danger mt-2";
-  errorDiv.id = "errorBoard";
-  errorDiv.innerText = err.message;
-  page.appendChild(errorDiv);
+ 
+  if(document.getElementById("errorBoard") == null){
+    const errorDiv = document.createElement("div");
+    errorDiv.className = "alert alert-danger mt-2";
+    errorDiv.id = "errorBoard";
+    errorDiv.innerText = err.message;  
+    page.appendChild(errorDiv);
+  }else{
+    document.querySelector("#errorBoard").innerText = err.message;
+  }
+  
 };
 
 export default PrintError;
