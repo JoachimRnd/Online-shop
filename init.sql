06fcccd4a2879350ed78f4d6903b1a204b6be2f5
Amaury De Tremerie <amaury.detremerie@student.vinci.be>
2021-03-09 18:48:12 +0100

    refactor: faute de frappe

--- init.sql
+++ init.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA project;
 CREATE TABLE project.addresses (
 	address_id SERIAL PRIMARY KEY,
 	street VARCHAR(100) NOT NULL,
-	buiding_number VARCHAR(100) NOT NULL,
+	building_number VARCHAR(100) NOT NULL,
 	unit_number VARCHAR(100) NULL,
 	postcode VARCHAR(100) NOT NULL,
 	commune VARCHAR(100) NOT NULL,
@@ -27,22 +27,21 @@ CREATE TABLE project.users (
 INSERT INTO project.addresses VALUES (DEFAULT, 'street', 'building_number', 'unit_number', 'postcode', 'commune', 'country');
 
 INSERT INTO project.users VALUES (DEFAULT, 'test', '$2a$10$qSBZrtuxTN3tM///i5rM8Opd3ioOk2vG.olTPH/UZqP8rjCtg19Zm', 'last_name', 'first_name', 1, 'email', NOW(), 'true', '0');
-
+/*
 SELECT * FROM project.addresses;
 
-/*
-CREATE TABLE projet.visit_requests (
+CREATE TABLE project.visit_requests (
 	visit_request_id SERIAL PRIMARY KEY,
 	request_date TIMESTAMP NOT NULL,
 	time_slot TEXT NOT NULL,
-	adress INTEGER REFERENCES projet.addresses (address_id) NULL,
+	adress INTEGER REFERENCES project.addresses (address_id) NULL,
 	status INTEGER NOT NULL,
 	chosen_date_time TIMESTAMP NULL,
 	cancellation_reason TEXT NULL,
 	customer INTEGER REFERENCES project.users (user_id) NOT NULL
 );
 
-CREATE TABLE projet.furniture_types (
+CREATE TABLE project.furniture_types (
 	type_id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL
 );
@@ -50,8 +49,8 @@ CREATE TABLE projet.furniture_types (
 CREATE TABLE project.furniture (
 	furniture_id SERIAL PRIMARY KEY,
 	description TEXT NOT NULL,
-	type INTEGER REFERENCES project.furniture_type (type_id) NOT NULL,
-	visit_request INTEGER REFERENCES projet.visit_requests (visit_request_id) NOT NULL,
+	type INTEGER REFERENCES project.furniture_types (type_id) NOT NULL,
+	visit_request INTEGER REFERENCES project.visit_requests (visit_request_id) NOT NULL,
 	purchase_price DECIMAL NULL,
 	withdrawal_date TIMESTAMP NULL,
 	selling_price DECIMAL NULL,
@@ -62,20 +61,20 @@ CREATE TABLE project.furniture (
 	withdrawal_date TIMESTAMP NULL,
 	buyer INTEGER REFERENCES project.users (user_id) NOT NULL,
 	condition INTEGER NULL,
-	unregistered_buyer_email VARCHAR(100) NULL
-	--favorite_picture INTEGER REFERENCES project.pictures (picture_id) NULL
+	unregistered_buyer_email VARCHAR(100) NULL,
+	--favourite_picture INTEGER REFERENCES project.pictures (picture_id) NULL
 );
 
-CREATE TABLE projet.pictures (
+CREATE TABLE project.pictures (
 	picture_id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	visible_for_everyone BOOLEAN NOT NULL,
 	furniture INTEGER REFERENCES project.furniture (furniture_id) NOT NULL,
 	scrolling_picture BOOLEAN NOT NULL
 );
-*/
+
 --ALTER TABLE furniture ADD favorite_picture INTEGER FOREIGN KEY REFERENCES project.pictures(picture_id) NULL;
-/*
+
 CREATE TABLE project.options (
 	option_id SERIAL PRIMARY KEY,
 	buyer INTEGER REFERENCES project.users (user_id) NOT NULL,
@@ -83,4 +82,5 @@ CREATE TABLE project.options (
 	duration INTEGER NOT NULL,
 	date TIMESTAMP NOT NULL,
 	status INTEGER NOT NULL
-);*/
+);
+*/
