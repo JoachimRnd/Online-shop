49c92b04e816ea1de2c486be3c56aca19c57d552
Lorine Etienne <lorine.etienne@student.vinci.be>
2021-03-26 12:03:45 +0100

    modifications DAOOptionImpl

--- src/main/java/be/vinci/pae/services/DAOOptionImpl.java
+++ src/main/java/be/vinci/pae/services/DAOOptionImpl.java
@@ -1,19 +1,32 @@
 package be.vinci.pae.services;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import be.vinci.pae.domain.FurnitureFactory;
 import be.vinci.pae.domain.OptionDTO;
 import be.vinci.pae.domain.OptionFactory;
+import be.vinci.pae.domain.UserFactory;
 import be.vinci.pae.utils.FatalException;
 import jakarta.inject.Inject;
 
 public class DAOOptionImpl implements DAOOption {
 
   private PreparedStatement selectAllOptions;
+  private PreparedStatement selectUserId;
+  private PreparedStatement selectFurnitureId;
   private String querySelectAllOptions;
+  private String queryAddOption;
+  private String querySelectUserId;
+  private String querySelectFurnitureId;
 
+  @Inject
+  private FurnitureFactory furnitureFactory;
+  @Inject
+  private UserFactory userFactory;
   @Inject
   private OptionFactory optionFactory;
   @Inject
@@ -22,7 +35,11 @@ public class DAOOptionImpl implements DAOOption {
 
   public DAOOptionImpl() {
     querySelectAllOptions = "SELECT o.option_id, o.buyer, o.furniture, o.duration, o.date"
-        + "o.status FROM project.options o";
+        + "o.status FROM project.options o;";
+    queryAddOption = "INSERT INTO project.options (option_id, buyer, furniture, duration, date"
+        + "status VALUES (DEFAULT, ?, ?, ?, ?, ?);";
+    querySelectUserId = "TODO";
+    querySelectFurnitureId = "TODO";
   }
 
   @Override
@@ -42,9 +59,37 @@ public class DAOOptionImpl implements DAOOption {
     }
   }
 
-  public int addOption(OptionDTO picture) {
+  public int addOption(OptionDTO option) {
     // TODO
-    return 42;
+    int optionId = -1;
+    try {
+      PreparedStatement addOption = this.dalServices.getPreparedStatement(queryAddOption);
+      if (selectUserId == null && selectFurnitureId == null) {
+        selectFurnitureId = this.dalServices.getPreparedStatement(querySelectFurnitureId);
+        selectUserId = this.dalServices.getPreparedStatement(querySelectUserId);
+      }
+      selectFurnitureId.setInt(1, option.getFurnitureId());
+      selectUserId.setInt(1, option.getBuyerId());
+      ResultSet rsFurniture = selectFurnitureId.executeQuery();
+      ResultSet rsUser = selectUserId.executeQuery();
+      if (rsFurniture != null && rsUser != null) {
+        addOption.setInt(2, option.getBuyerId());
+        addOption.setInt(3, option.getFurnitureId());
+        addOption.setInt(4, option.getDuration());
+        // TODO setDate to LocalDate --> is that correct ?
+        addOption.setDate(5, Date.valueOf(option.getDate()));
+        addOption.setInt(6, option.getStatus());
+      }
+      try (ResultSet rs = addOption.getGeneratedKeys()) {
+        if (rs.next()) {
+          optionId = rs.getInt(1);
+        }
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.println("Database error : addOption");
+    }
+    return optionId;
   }
 
   public OptionDTO selectOptionByID(int id) {
