68b3ecfc2e7b61c3788c5b4eedcf24a2681eb471
Amaury De Tremerie <amaury.detremerie@student.vinci.be>
2021-03-30 15:32:00 +0200

    feat: backend option

--- src/main/java/be/vinci/pae/services/DAOOption.java
+++ src/main/java/be/vinci/pae/services/DAOOption.java
@@ -1,15 +1,20 @@
 package be.vinci.pae.services;
 
-import java.util.List;
-
 import be.vinci.pae.domain.OptionDTO;
+import java.util.List;
 
 public interface DAOOption {
 
   List<OptionDTO> selectAllOptions();
-  
+
   int addOption(OptionDTO picture);
-  
+
   OptionDTO selectOptionByID(int id);
+
+  List<OptionDTO> selectOptionsOfFurniture(int idFurniture);
+
+  List<OptionDTO> selectOptionsOfBuyer(int idBuyer);
+
+  OptionDTO selectOptionsOfBuyerFromFurniture(int idBuyer, int idFurniture);
 
 }
