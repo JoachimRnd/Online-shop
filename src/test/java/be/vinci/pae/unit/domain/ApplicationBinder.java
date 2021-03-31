e1ca70a20efb04dd4188a37b6eefb4b400b9f2ff
Lorine Etienne <lorine.etienne@student.vinci.be>
2021-03-31 21:36:27 +0200

    modify test to mockito testing

--- src/test/java/be/vinci/pae/unit/domain/ApplicationBinder.java
+++ src/test/java/be/vinci/pae/unit/domain/ApplicationBinder.java
@@ -1,23 +1,20 @@
 package be.vinci.pae.unit.domain;
 
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import be.vinci.pae.domain.UserFactory;
 import be.vinci.pae.domain.UserFactoryImpl;
-import be.vinci.pae.domain.UserUCC;
-import be.vinci.pae.domain.UserUCCImpl;
-import be.vinci.pae.services.DAOUser;
-import be.vinci.pae.services.DalServices;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.ext.Provider;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 @Provider
 public class ApplicationBinder extends AbstractBinder {
 
+
   @Override
   protected void configure() {
     bind(UserFactoryImpl.class).to(UserFactory.class).in(Singleton.class);
-    bind(DAOUserMock.class).to(DAOUser.class).in(Singleton.class);
-    bind(UserUCCImpl.class).to(UserUCC.class).in(Singleton.class);
-    bind(DalServicesMock.class).to(DalServices.class).in(Singleton.class);
+    // bind(DalServicesMock.class).to(DalServices.class).in(Singleton.class);
+    // bind(DAOUserMock.class).to(DAOUser.class).in(Singleton.class);
+
   }
 }
--- src/test/java/be/vinci/pae/unit/domain/UserUCCTest.java
+++ src/test/java/be/vinci/pae/unit/domain/UserUCCTest.java
@@ -1,42 +1,89 @@
 package be.vinci.pae.unit.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import be.vinci.pae.domain.UserUCC;
-import be.vinci.pae.utils.BusinessException;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import be.vinci.pae.domain.User;
+import be.vinci.pae.domain.UserDTO;
+import be.vinci.pae.domain.UserFactory;
+import be.vinci.pae.domain.UserUCCImpl;
+import be.vinci.pae.services.DAOUser;
+import be.vinci.pae.services.DalServices;
+import be.vinci.pae.utils.BusinessException;
 
 public class UserUCCTest {
 
-  private UserUCC userUCC;
+
+  UserFactory userFact;
+
+  @Mock
+  DalServices dalServices;
+
+  @Mock
+  DAOUser userdao;
+
+  @InjectMocks
+  UserUCCImpl userUCC;
+
 
   @BeforeEach
   void initAll() {
+    MockitoAnnotations.initMocks(this);
     ServiceLocator locator = ServiceLocatorUtilities.bind(new ApplicationBinder());
-    this.userUCC = locator.getService(UserUCC.class);
+    // this.userUCC = locator.getService(UserUCC.class);
+    this.userFact = locator.getService(UserFactory.class);
+
   }
 
+  @DisplayName("test if user is null")
   @Test
   public void userUCCNotNullTest() {
     assertNotNull(this.userUCC);
   }
 
+  @DisplayName("test login with both good username and password")
   @Test
   public void loginGoodLoginGoodPasswordTest() {
-    assertNotNull(userUCC.login("login", "password"));
+
+    UserDTO userToConnect = userFact.getUser();
+    userToConnect.setPassword(((User) userToConnect).hashPassword("test"));
+    userToConnect.setUsername("test");
+
+    Mockito.when(userdao.getUserByUsername("test")).thenReturn(userToConnect);
+    UserDTO user = userUCC.login("test", "test");
+    assertEquals(userToConnect, user);
   }
 
+  @DisplayName("test login with bad username and good password")
   @Test
   public void loginBadLoginGoodPasswordTest() {
-    assertThrows(BusinessException.class, () -> userUCC.login("bad", "password"));
+    UserDTO userToConnect = userFact.getUser();
+    userToConnect.setPassword(((User) userToConnect).hashPassword("test"));
+    userToConnect.setUsername("test");
+    Mockito.when(userdao.getUserByUsername("test")).thenReturn(userToConnect);
+    assertThrows(BusinessException.class, () -> userUCC.login("chouquettes", "test"));
   }
 
+  @DisplayName("test login with good username and bad password")
   @Test
   public void loginGoodLoginBadPasswordTest() {
+    UserDTO userToConnect = userFact.getUser();
+    userToConnect.setPassword(((User) userToConnect).hashPassword("test"));
+    userToConnect.setUsername("test");
+
+    Mockito.when(userdao.getUserByUsername("test")).thenReturn(userToConnect);
     assertThrows(BusinessException.class, () -> userUCC.login("login", "bad"));
   }
+
+  // TODO TESTER methodes register() et validate()
+
 }
