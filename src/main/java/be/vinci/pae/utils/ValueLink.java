6f62b7606e537399b57230801acec0665cfde12e
Olivier DEWALQUE <olivier.dewalque@student.vinci.be>
2021-04-05 14:54:24 +0200

    PMD warnings correction

--- src/main/java/be/vinci/pae/utils/ValueLink.java
+++ src/main/java/be/vinci/pae/utils/ValueLink.java
@@ -2,20 +2,20 @@ package be.vinci.pae.utils;
 
 public interface ValueLink {
 
-  public enum UserType {
+  enum UserType {
     client, antiquaire, admin
   }
 
-  public enum FurnitureCondition {
+  enum FurnitureCondition {
     propose, ne_convient_pas, achete, emporte_par_patron, en_restauration, en_magasin, en_vente,
     retire_de_vente, en_option, vendu, reserve, livre, emporte_par_client
   }
 
-  public enum OptionStatus {
+  enum OptionStatus {
     annulee, en_cours, finie
   }
 
-  public enum VisitRequestStatus {
+  enum VisitRequestStatus {
     annulee, en_attente, confirmee
   }
 }
