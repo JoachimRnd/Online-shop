9d96393b03bb8193f2a8a15212cedd0ea7b8b549
Olivier DEWALQUE <olivier.dewalque@student.vinci.be>
2021-04-13 17:02:59 +0200

    Refactor delivery date and WithdrawalDate in sold condition

--- webApp/src/Components/Furniture/FurnitureAdmin.js
+++ webApp/src/Components/Furniture/FurnitureAdmin.js
@@ -165,17 +165,28 @@ const FurnitureAdmin = async(f) => {
   let btnReturn = document.querySelector("#btnReturn");
   btnReturn.addEventListener("click", () => RedirectUrl("/search"));
 
+  const user = getUserSessionData();
 
   if(f == null){
     let queryString = window.location.search;
     let urlParams = new URLSearchParams(queryString);
     let id = urlParams.get("id");
-    try {
-      furniture = await callAPI(API_BASE_URL + id , "GET", undefined);
-    } catch (err) {
-      console.error("FurnitureUser::GetFurnitureByID", err);
-      PrintError(err);
+    if(user && user.token == "admin"){
+      try {
+        furniture = await callAPI(API_BASE_URL_ADMIN + id , "GET", user.token);
+      } catch (err) {
+        console.error("FurnitureUser::GetFurnitureByID", err);
+        PrintError(err);
+      }
+    }else {
+      try {
+        furniture = await callAPI(API_BASE_URL + id , "GET", undefined);
+      } catch (err) {
+        console.error("FurnitureUser::GetFurnitureByID", err);
+        PrintError(err);
+      }
     }
+
   }else{
     furniture = f;
   }
@@ -196,6 +207,9 @@ const FurnitureAdmin = async(f) => {
   onCheckOption();
 
   let conditions = document.querySelector("#conditions");
+  if(conditions.value == "vendu"){
+    onSold();
+  }
   if(conditions.value == "en_vente"){
     onSale();
   } else if (conditions.value == "achete") {
@@ -207,6 +221,9 @@ const FurnitureAdmin = async(f) => {
 
   conditions.addEventListener("change",(e)=>{
     onFurniture();
+    if(conditions.value == "vendu"){
+      onSold();
+    }
     if(conditions.value == "en_vente"){
       onSale();
     } else if (conditions.value == "achete") {
@@ -276,7 +293,9 @@ const onPurchase = () => {
 
   let withdrawalDateFromCustomer = document.querySelector("#withdrawalDateFromCustomer");
   withdrawalDateFromCustomer.innerHTML = `<input class="form-control" id="inputWithdrawalDateFromCustomer" type="date"/>`;
+}
 
+const onSold = () => {
   let deliveryDate = document.querySelector("#deliveryDate");
   deliveryDate.innerHTML = `<input class="form-control" id="inputDeliveryDate" type="date"/>`;
 
