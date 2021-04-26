9498269d883f4758dd39a064d51801ce536dbe33
Amaury De Tremerie <amaury.detremerie@student.vinci.be>
2021-04-26 22:20:40 +0200

    feat : Recherche sur mes meubles

--- webApp/src/Components/Furniture/FurnitureUserListPage.js
+++ webApp/src/Components/Furniture/FurnitureUserListPage.js
@@ -4,35 +4,77 @@ import { callAPI } from "../../utils/api.js";
 import PrintError from "../PrintError.js";
 const API_BASE_URL = "/api/furniture/";
 
-let page = document.querySelector("#page");
-let furniturePage;
+let furnitureBuyByUser, furnitureSellByUser;
 
-const FurnitureUserListPage = async () => {  
-  furniturePage += `
-  <h4 id="pageTitle">Mes meubles</h4>
-    <div class="row">
+
+let furniturePage = `<h4 id="pageTitle">Mes meubles</h4>
+<div class="form-group">
+<input class="col-auto" type="text" id="type" placeholder="Type">
+<input class="col-auto" type="number" id="price" placeholder="Prix max">
+<button class="btn btn-success" id="searchBtn" type="submit">Rechercher</button>
+</div>
+<div class="row">
     <div class="col-6">
-      <div class="form-group">
-          <label for="furniture_buyed">Meuble acheté par le client</label>
-          <div id="furniture_buyed"></div>
-      </div>
+        <div class="form-group">
+            <label for="furniture_buyed">Meuble acheté</label>
+            <div id="furniture_buyed"></div>
+        </div>
     </div>
     <div class="col-6">
-      <div class="form-group">
-          <label for="furniture_selled">Meuble vendu par le client</label>
-          <div id="furniture_selled"></div>
-      </div>
+        <div class="form-group">
+            <label for="furniture_selled">Meuble vendu</label>
+            <div id="furniture_selled"></div>
+        </div>
     </div>
-  </div>`;
+</div>`;
+
+const FurnitureUserListPage = async () => {
+    let page = document.querySelector("#page");
     page.innerHTML = furniturePage;
 
     const user = getUserSessionData();
-    let furnitureBuyByUser = await callAPI(API_BASE_URL + "furniturebuyby/" + user.user.id , "GET", user.token);
+    furnitureBuyByUser = await callAPI(API_BASE_URL + "furniturebuyby/" + user.user.id , "GET", user.token);
     onFurnitureBuy(furnitureBuyByUser);
-    let furnitureSellByUser = await callAPI(API_BASE_URL + "furnituresellby/" + user.user.id , "GET", user.token);
+    furnitureSellByUser = await callAPI(API_BASE_URL + "furnituresellby/" + user.user.id , "GET", user.token);
     onFurnitureSell(furnitureSellByUser);
+
+    let searchBtn = document.querySelector('#searchBtn');
+    searchBtn.addEventListener("click", onSubmitSearch);
 };
 
+const onSubmitSearch = async () => {
+    let typeSearchSell = onTypeSearch(furnitureSellByUser);
+    let typeSearchBuy = onTypeSearch(furnitureBuyByUser);
+    let priceSearchSell = onPriceSearch(typeSearchSell);
+    let priceSearchBuy = onPriceSearch(typeSearchBuy);
+
+    onFurnitureBuy(priceSearchBuy);
+    onFurnitureSell(priceSearchSell);
+};
+
+const onTypeSearch = (data) => {
+    let typeField = document.querySelector('#type');
+    let typeValue = typeField.value;
+    if(typeValue.length === 0) {
+        return data;
+    }
+    return data.filter(data => {
+        let regex = new RegExp(`^${typeValue}`, 'gi');
+        return data.type.name.match(regex);
+    });
+};
+
+const onPriceSearch = (data) => {
+    let priceField = document.querySelector('#price');
+    let priceValue = priceField.value;
+    if(priceValue.length === 0) {
+        return data;
+    }
+    return data.filter(data => {
+        console.log(data.sellingPrice);
+        return data.sellingPrice >= priceValue;
+    });
+};
 
 const onFurnitureBuy = (data) => {
   let furnitureBuyed = document.querySelector('#furniture_buyed');
@@ -43,7 +85,6 @@ const onFurnitureBuy = (data) => {
                   <tr>
                       <th>Description</th>
                       <th>Type</th>
-                      <th>Etat</th>
                   </tr>
               </thead>
               <tbody>`;
@@ -51,7 +92,6 @@ const onFurnitureBuy = (data) => {
       display +=  `<tr>
                                   <td><a id="furnitureBuyed${element.id}" href="" target="_blank">${element.description}</a></td>
                                   <td>${element.type.name}</td>
-                                  <td>${element.condition}</td>
                               </tr>`;
   });
   display += `</tbody></table></div>`;
@@ -76,7 +116,6 @@ const onFurnitureSell = (data) => {
                   <tr>
                       <th>Description</th>
                       <th>Type</th>
-                      <th>Etat</th>
                   </tr>
               </thead>
               <tbody>`;
@@ -84,7 +123,6 @@ const onFurnitureSell = (data) => {
       display +=  `<tr>
                                   <td><a id="furnitureSelled${element.id}" href="" target="_blank">${element.description}</a></td>
                                   <td>${element.type.name}</td>
-                                  <td>${element.condition}</td>
                               </tr>`;
   });
   display += `</tbody></table></div>`;
